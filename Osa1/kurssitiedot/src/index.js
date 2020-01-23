979c375aea887ef0f8cc9a624ca6fd20d94d18a3
mvtteus <matias.jarvinen@helsinki.fi>
2020-01-23 12:51:59 +0200

    1.4 done

--- Osa1/kurssitiedot/src/index.js
+++ Osa1/kurssitiedot/src/index.js
@@ -10,53 +10,57 @@ const Header = (props) => {
       </div>
   )
 }
-const Part = ({nimi, tehtavia}) => {
+const Part = (props) => {
   return (
       <div>
           <p>
-            {nimi} {tehtavia}
+            {props.nimi} {props.tehtavia}
           </p>
       </div>
   )
 } 
-const Content = ({osa1, osa2, osa3}) => {
+const Content = (props) => {
+  const [eka, toka, kolmas] = props.taulukko
   return (
     <div>
-      <Part nimi = {osa1.name} tehtavia = {osa1.exercises}/>
-      <Part nimi = {osa2.name} tehtavia = {osa2.exercises}/>
-      <Part nimi = {osa3.name} tehtavia = {osa3.exercises}/>
+      <Part nimi = {eka.name} tehtavia = {eka.exercises}/>
+      <Part nimi = {toka.name} tehtavia = {toka.exercises}/>
+      <Part nimi = {kolmas.name} tehtavia = {kolmas.exercises}/>
     </div>
   )
 } 
 const Total = (props) => {
+  const [eka, toka, kolmas] = props.taulukko
   return (
     <div>
         <p>
-          Number of exercises {props.exercises.reduce(function (a, b) { return a + b; }, 0)}
+          Number of exercises {eka.exercises + toka.exercises + kolmas.exercises}
         </p>
     </div>
   )
 }
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content osa1 = {part1} osa2 = {part2} osa3 = {part3} />
-      <Total exercises = {[part1.exercises, part2.exercises, part3.exercises]}/>
+      <Content taulukko = {parts} />
+      <Total taulukko = {parts}/>
     </div>
   )
 }
