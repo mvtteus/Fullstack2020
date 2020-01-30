6139199523c2015a94f6e9843ed4b1c8dbdee5b3
mvtteus <matias.jarvinen@helsinki.fi>
2020-01-30 17:47:54 +0200

    update

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -6,19 +6,40 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('button clicked', EventTarget)
+    const nameObject = {
+      name: newName
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit = {addName}>
         <div>
-          name: <input />
+          name: <input value = {newName} onChange = {handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person, i) => 
+          <li key={person.name}>
+         {person.name}
+          </li>
+          )}
+      </ul>
     </div>
   )
 
