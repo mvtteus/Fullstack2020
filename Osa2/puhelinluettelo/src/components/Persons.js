3cec7a742b99038d6c86a6338d8a3c77c1920695
mvtteus <matias.jarvinen@helsinki.fi>
2020-01-31 17:51:44 +0200

    2.10 done

--- Osa2/puhelinluettelo/src/components/Persons.js
+++ Osa2/puhelinluettelo/src/components/Persons.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Persons = ({persons, newSearch}) => {
     return (
         <div>
-          {persons.filter(i => i.name.includes(newSearch)).map((person, i) => 
+          {persons.filter(i => i.name.toLowerCase().includes(newSearch.toLowerCase())).map((person, i) => 
           <p key={person.name}>
          {person.name} {person.number}
           </p>
