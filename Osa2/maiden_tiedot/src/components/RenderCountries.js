9ba3431b5f478c580eca72788be7cf2f6788c60b
mvtteus <matias.jarvinen@helsinki.fi>
2020-02-03 15:37:45 +0200

    2.12 done

--- Osa2/maiden_tiedot/src/components/RenderCountries.js
+++ Osa2/maiden_tiedot/src/components/RenderCountries.js
@@ -1,21 +1,42 @@
 import React from 'react'
 
-const RenderCountries = ({countries, newSearch}) => {
-    if (countries.filter(i => i.name.toLowerCase().includes(newSearch.toLowerCase())).length <= 10) {
-        return (
+const RenderSingleCountry = ({country}) => {
+    return (
         <div>
-          {countries.filter(i => i.name.toLowerCase().includes(newSearch.toLowerCase())).map((country, i) => 
-          <p key={i}>
-         {country.name}
-          </p>
-          )}
+            <h1>{country.name}</h1>
+            <p>capital {country.capital}</p>
+            <p>population {country.population}</p> 
+            <h2>languages</h2>
+            <ul>
+                {country.languages.map((lang, i) => 
+                    <li key = {lang.iso639_1}>
+                        {lang.name}
+                    </li>)}
+            </ul>
+            <img src = {country.flag} width="100" height="100"/>
         </div>
+    )
+}
+const RenderCountries = ({countries, newSearch}) => {
+    if (countries.filter(i => i.name.toLowerCase().includes(newSearch.toLowerCase())).length === 1) {
+        return (
+            <RenderSingleCountry country = {countries.filter(i => i.name.toLowerCase().includes(newSearch.toLowerCase()))[0]}/>
         )
+    } else if (countries.filter(i => i.name.toLowerCase().includes(newSearch.toLowerCase())).length <= 10 && countries.filter(i => i.name.toLowerCase().includes(newSearch.toLowerCase())).length >= 2) {
+        return (
+            <div>
+              {countries.filter(i => i.name.toLowerCase().includes(newSearch.toLowerCase())).map((country, i) => 
+              <p key={i}>
+             {country.name}
+              </p>
+              )}
+            </div>
+            )
     } else {
         return (
             <p>Too many matches, specify another filter</p>
-        )
-    } 
+        ) 
+    }
 }
 
 export default RenderCountries
