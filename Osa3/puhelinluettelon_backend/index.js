c85a17ef9cc690a12d99497322bddca600945484
mvtteus <matias.jarvinen@helsinki.fi>
2020-02-06 16:12:43 +0200

    3.10 preparation

--- Osa3/puhelinluettelon_backend/index.js
+++ Osa3/puhelinluettelon_backend/index.js
@@ -100,8 +100,7 @@ app.post('/api/persons', (request, response) => {
   persons.concat(person)
   response.json(person)
 })
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
